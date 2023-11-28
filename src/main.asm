f0e6bcc79377960e832469db066274a235128193
vyytx <dobeender@gmail.com>
2023-11-28 22:45:50 +0800

    hide cursor when in game

--- src/main.asm
+++ src/main.asm
@@ -6,18 +6,28 @@ StartGameMsg BYTE "Game Start!", 0
 GoodByeMsg	 BYTE "Thanks for playing", 0
 SettingMsg	 BYTE "Open Setting Dialogue",0
 
+stdoutHandle	 DWORD ?
+IngameCursorInfo CONSOLE_CURSOR_INFO <1, 0>
+NormalCursorInfo CONSOLE_CURSOR_INFO <?, ?>
+
 .code
 extern HandleStartMenu: PROC
 extern ShowMap: PROC
-main PROC
 
-	call ShowMap
+main PROC
+	invoke GetStdHandle, STD_OUTPUT_HANDLE
+	mov stdoutHandle, eax
+	invoke GetConsoleCursorInfo, stdoutHandle, OFFSET NormalCursorInfo
+	invoke SetConsoleCursorInfo, stdoutHandle, OFFSET IngameCursorInfo
+
+	; call ShowMap
 	
-	;call HandleStartMenu
+	; call HandleStartMenu
 
 	.IF eax == 2
 		mov edx, OFFSET GoodByeMsg
 		call WriteString
+		invoke SetConsoleCursorInfo, stdoutHandle, OFFSET NormalCursorInfo
 		exit
 	.ELSEIF eax == 0
 		mov edx, OFFSET StartGameMsg
