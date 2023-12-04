8f14e3fddb1d83c0f67198b1444819e157530e1d
qiaoyi213 <linqiaoyi213@gmail.com>
2023-12-04 15:06:07 +0800

    update game.asm

--- src/Game.asm
+++ src/Game.asm
@@ -14,16 +14,20 @@ Game PROC USES eax ebx
 
     mov bx, 0
     mov ecx, 120
+
+
 game_update_loop:
-    mov eax, 400
+    mov eax, 50
     call Delay
     mov maxV, bx
     call Clrscr
-    invoke ShowPart, 0,0, maxV, 30
+    invoke ShowPart, 0, 0, maxV, 29
+
     
     inc bx
     loop game_update_loop
 
+    
     ret
 Game ENDP
 
