9a189ff45ad2f93d630464706d5f5e0005adcdc2
vyytx <dobeender@gmail.com>
2023-12-04 16:03:09 +0800

    initialize these

---
+++ src/Game.asm
@@ -0,0 +1,12 @@
+INCLUDE Ervine32.inc
+
+.data
+Level	BYTE	0
+
+
+.code
+
+
+
+
+END
---
+++ src/menus/StartMenu.asm
@@ -0,0 +1,9 @@
+INCLUDE Ervine32.inc
+INCLUDE WINDOWS.inc
+
+.data
+
+.code
+StartMenu_init PROC
+
+StartMenu_init ENDP
