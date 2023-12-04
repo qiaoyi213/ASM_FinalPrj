19ee92c4f90d591d00d86151a41012aee8a453a5
vyytx <dobeender@gmail.com>
2023-12-04 14:46:46 +0800

    basic window

---
+++ src/Window.asm
@@ -0,0 +1,73 @@
+INCLUDE Ervine32.inc
+INCLUDE WINDOWS.inc
+
+Window_Process PROTO, :HWND, :UINT, :WPARAM, :LPARAM 
+
+.data
+
+ClassName		BYTE		"SimpleWinClass", 0
+windowTitle		BYTE		"The Game", 0
+hInstance		HINSTANCE	?
+hwnd			HWND		?
+windowClass		WNDCLASSEX	<30h,?,?,0,0,?,?,?,?,0,OFFSET ClassName,?>
+msg				MSG			<?>
+
+.code
+
+Window_init PROC
+	invoke  GetModuleHandle, NULL
+	mov     hInstance, eax
+
+	mov     windowClass.style, CS_HREDRAW or CS_VREDRAW ; re draw when window resized
+
+	mov     windowClass.lpfnWndProc, OFFSET Window_Process
+
+	mov     eax, hInstance
+	mov     windowClass.hInstance, eax
+
+	mov     windowClass.hbrBackground, COLOR_WINDOW+1
+
+	; Since no icon, disable first
+	; invoke  LoadIcon,NULL,IDI_APPLICATION   ;取得圖示代碼
+	; mov     wc.hIcon,eax                    ;存入圖示代碼
+	; mov     wc.hIconSm,eax                  ;存入小圖示代碼
+
+	invoke  LoadCursor, NULL, IDC_ARROW					;取得游標代碼
+	mov     windowClass.hCursor,eax						;存入游標代碼
+
+	invoke  RegisterClassEx, OFFSET windowClass			;註冊視窗類別
+
+	invoke  CreateWindowEx, NULL, OFFSET ClassName, OFFSET windowTitle,\
+			WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,\
+			CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL
+	mov     hwnd, eax
+
+	invoke  ShowWindow, hwnd, SW_SHOWDEFAULT
+	invoke  UpdateWindow, hwnd
+ 
+gt_msg:
+	invoke  GetMessage,OFFSET msg,NULL,0,0
+	or      eax,eax
+	jz      wm_qut
+	invoke  TranslateMessage,OFFSET msg
+	invoke  DispatchMessage,OFFSET msg
+	jmp     gt_msg
+wm_qut:
+	mov     eax,msg.wParam                  ;程式結束
+	invoke  ExitProcess,eax
+	ret
+Window_init ENDP
+
+Window_Process PROC, hWnd: HWND, uMsg: UINT, wParam: WPARAM, lParam: LPARAM
+	.IF uMsg == WM_DESTROY
+		invoke  PostQuitMessage, NULL
+		mov eax, 0
+		ret
+	.ENDIF
+msg_process:
+    invoke  DefWindowProc, hWnd, uMsg, wParam, lParam
+	
+	ret
+Window_Process ENDP
+
+END
--- src/main.asm
+++ src/main.asm
@@ -1,67 +1,14 @@
 INCLUDE Ervine32.inc
 INCLUDE WINDOWS.inc
 
-WndProc PROTO	:HWND, :UINT, :WPARAM, :LPARAM 
-
 .data
 
-ClassName       DB          "SimpleWinClass",0  ;視窗類別名稱
-AppName         DB          "Our First Window",0
-hInstance       HINSTANCE   ?                   ;模組代碼
-hwnd            HWND        ?                   ;視窗代碼
-CommandLine     LPSTR       ?                   ;命令列位址
-wc      WNDCLASSEX      <30h,?,?,0,0,?,?,?,?,0,OFFSET ClassName,?>
-msg     MSG             <?>
-
 .code
+	extern Window_init: PROC
 
 main PROC
-	INVOKE  GetModuleHandle,NULL            ;取得模組代碼
-	mov     hInstance,eax
-	INVOKE  GetCommandLine                  ;取得命令列字串參數位址
-	mov     CommandLine,eax
-	mov     wc.style,CS_HREDRAW or CS_VREDRAW
-	mov     wc.lpfnWndProc,OFFSET WndProc
-	mov     eax,hInstance
-	mov     wc.hInstance,eax
-	mov     wc.hbrBackground,COLOR_WINDOW+1
-	INVOKE  LoadIcon,NULL,IDI_APPLICATION   ;取得圖示代碼
-	mov     wc.hIcon,eax                    ;存入圖示代碼
-	mov     wc.hIconSm,eax                  ;存入小圖示代碼
-	INVOKE  LoadCursor,NULL,IDC_ARROW       ;取得游標代碼
-	mov     wc.hCursor,eax                  ;存入游標代碼
-	INVOKE  RegisterClassEx,OFFSET wc       ;註冊視窗類別
-
-	INVOKE  CreateWindowEx,NULL,OFFSET ClassName,OFFSET AppName,\
-			WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,\
-			CW_USEDEFAULT,CW_USEDEFAULT,NULL,NULL,hInstance,NULL
-	mov     hwnd,eax
-	INVOKE  ShowWindow,hwnd,SW_SHOWDEFAULT
-	INVOKE  UpdateWindow,hwnd
- 
-gt_msg:
-	INVOKE  GetMessage,OFFSET msg,NULL,0,0
-	or      eax,eax
-	jz      wm_qut
-	INVOKE  TranslateMessage,OFFSET msg
-	INVOKE  DispatchMessage,OFFSET msg
-	jmp     gt_msg
-wm_qut:
-	mov     eax,msg.wParam                  ;程式結束
-    INVOKE  ExitProcess,eax
+	call Window_init
 	ret
 main ENDP
-
-WndProc PROC    hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
-        cmp     uMsg,WM_DESTROY
-        jne     msg_process
-        INVOKE  PostQuitMessage,NULL
-        jmp     exit
-msg_process:
-        INVOKE  DefWindowProc,hWnd,uMsg,wParam,lParam
-        ret
-exit:   xor     eax,eax
-        ret
-WndProc ENDP
 
 END main
